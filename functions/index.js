625233e731c4e1c90e6b8598aedde28ec42c77ca
Murilo Beltrame Correia <murilo.correia@raizen.com>
2020-03-13 00:32:50 -0300

    add clain to user

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,21 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+exports.addAdminRole = functions.https.onCall(function(data, context) {
+    //get user and add custom claim (admin)
+    return admin.auth().getUserByEmail(data.email).then(function(user) {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true
+        });
+    }).then(function() {
+        return {
+            message: `Sucess! ${data.email} has been made an admin.`
+        }
+    }).catch(function(error) {
+        return error;
+    });
+});
